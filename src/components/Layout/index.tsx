4ae742c06500ae90c303e58035a3164ad4991000
geninhocell <geninhocell@gmail.com>
2022-03-19 11:43:24 -0300

    added: import styles

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'gatsby';
 import React, { ReactNode } from 'react';
+import { Container } from './styles';
 
 type LayoutProps =  {
     pageTitle: string;
@@ -8,18 +9,18 @@ type LayoutProps =  {
 
 export function Layout({ pageTitle, children }: LayoutProps) {
   return (
-    <div>
-    <title>{pageTitle}</title>
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav>
-    <main>
-      <h1>{pageTitle}</h1>
-      {children}
-    </main>
-  </div>
+    <Container>
+        <title>{pageTitle}</title>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </nav>
+        <main>
+            <h1>{pageTitle}</h1>
+            {children}
+        </main>
+    </Container>
   );
 }
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Layout } from '../../components/Layout';
+import { Container } from './styles';
 
 
 export default function About() {
   return (
-    <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </Layout>
+    <Container>
+      <Layout pageTitle="About Me">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
+    </Container>
   );
 }
